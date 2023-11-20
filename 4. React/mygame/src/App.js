213f2f8692d1f7943a1c11ddca4dd767a753efbe
arifiani1009 <arifiani.tc09@gmail.com>
2023-11-20 19:52:23 +0700

    Form Login React
    Component Form Login React

---
+++ 4. React/mygame/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginRegister from './components/LoginRegister/LoginRegister';
+
+function App() {
+  return (
+    <div>
+      <LoginRegister/>
+    </div>
+  );
+}
+
+export default App;
